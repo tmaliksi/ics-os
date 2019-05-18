adb9b0d233baa0290a1b17c978fbc21e8d364a8b
tmaliksi <tcmaliksi@up.edu.ph>
2019-05-18 19:42:44 +0800

    edit

--- ics-os/kernel/console/console.c
+++ ics-os/kernel/console/console.c
@@ -833,7 +833,6 @@ int console_execute(const char *str){
             time_systime.sec, time_systime.str_day);
       }else{
          char *date, *time;
-         u = strtok(0," ");
          date = u;
          u = strtok(0," ");
          time = u;
